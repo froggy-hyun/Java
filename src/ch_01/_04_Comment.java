855b0092a0da49b0b1943bd877f9af443a17bda1
froggy-hyun <1233day@naver.com>
2024-09-09 11:55:58 +0900

    init: ch_01 original sources

---
+++ src/ch_01/_04_Comment.java
@@ -0,0 +1,12 @@
+package ch_01;
+
+// 한줄 주석 : ctrl + /
+// 여러줄 주석 : ctrl + shift + /
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분전) 잠시후 행사가 진행됩니다. ");
+        //System.out.println("(5분전) 잠시후 행사가 진행됩니다. ");
+        System.out.println("그럼 식순을 진행하겠습니다. ");
+    }
+}
