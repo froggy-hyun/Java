50dc0bf796ccb1fb7c40ae4c930e6f3f594b5e5f
froggy-hyun <1233day@naver.com>
2024-09-26 16:06:14 +0900

    submit: task01

---
+++ src/task01/_Quiz_02.java
@@ -0,0 +1,12 @@
+package task01;
+
+import java.util.Scanner;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("키를 입력하시오: ");
+        int height = scanner.nextInt();
+        System.out.println("키가 " + height + " cm 이므로 탑승 " + ((height >= 120) ? "가능" : "불가능") + "합니다");
+    }
+}
