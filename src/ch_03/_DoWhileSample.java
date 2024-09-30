b85b66f0db09fba44c53a3b16d9950a16b0c1c1a
froggy-hyun <1233day@naver.com>
2024-09-30 12:06:01 +0900

    study: ch05

---
+++ src/ch_03/_DoWhileSample.java
@@ -0,0 +1,11 @@
+package ch_03;
+
+public class _DoWhileSample {
+    public static void main(String[] args) {
+        char c = 'a';
+        do {
+            System.out.print(c);
+            c = (char) (c + 1);
+        } while (c <= 'z');
+    }
+}
